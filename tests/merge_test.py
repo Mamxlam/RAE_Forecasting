fd0bc29a37803e0f76992ae90ef42be6f7720a3c
mamxlam <mamxlam@teiemt.gr>
2024-05-25 14:50:48 +0300

    Fixed and modified test file for merging files.

--- tests/merge_test.py
+++ tests/merge_test.py
@@ -3,9 +3,9 @@ import pandas as pd
 import seaborn as sns
 import os
 
-import debugpy
-debugpy.listen(5678)
-debugpy.wait_for_client()
+# import debugpy
+# debugpy.listen(5678)
+# debugpy.wait_for_client()
 
 
 def merge_dataframes(df1, df2):
@@ -33,6 +33,10 @@ def merge_dataframes(df1, df2):
         if df2[col].dtype == 'datetime64[ns]':
             df2[col] = df2[col].dt.strftime('%Y-%m-%d')
 
+    for col in df1.columns:
+        if df1[col].dtype == 'datetime64[ns]':
+            df1[col] = df1[col].dt.strftime('%Y-%m-%d')
+
     common_columns = df1.columns.intersection(df2.columns)
 
     merged_df = df1.merge(df2, on=common_columns.tolist(), how='outer')
@@ -51,10 +55,10 @@ def merge_dataframes_with_previous_files(file_paths, column_patterns):
     DataFrame: Merged DataFrame containing the first 13 columns of each DataFrame from all files.
     """
     merged_df = None
-    merged_df_temp = None
-    
+
     # Iterate over each file path
     for file_path in file_paths:
+        merged_df_temp = None
         print(f"\nLoading file : {file_path}")
         # Load Excel file into separate DataFrames
         # Fetch only first sheet as it is RES 
@@ -63,6 +67,7 @@ def merge_dataframes_with_previous_files(file_paths, column_patterns):
 
         # Merge in sheet level
         for sheet_name, df_sheet in df.items():
+            print(f"Number of rows detected: {len(df_sheet)}")
             # Filter according to pattern columns to keep
             df_sheet = df_sheet.filter(regex='|'.join(column_patterns))
             df_sheet['file'] = file_path
@@ -79,8 +84,59 @@ def merge_dataframes_with_previous_files(file_paths, column_patterns):
             merged_df = merged_df_temp
         else:
             merged_df = merge_dataframes(merged_df, merged_df_temp)
+        
+        print(f"Number of rows detected after merge: {len(merged_df)}")
 
     return merged_df
+
+def fillna_column(df, column_name, fillna_column_name):
+    """
+    Fill missing values in a column by using values from another column.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the columns.
+        column_name (str): The name of the column to fill missing values.
+        fillna_column_name (str): The name of the column to use for filling missing values.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with missing values filled in the specified column.
+    """
+    df[column_name] = df[column_name].fillna(df[fillna_column_name])
+    df.drop(fillna_column_name, axis=1, inplace=True)
+    return df
+
+def convert_to_datetime(df, columns=None):
+    """
+    Convert date-like columns in a DataFrame to datetime type.
+
+    Parameters:
+    df (DataFrame): Input DataFrame.
+    columns (list of str, optional): List of columns to convert. 
+                                     If None, attempts to convert all object type columns.
+
+    Returns:
+    DataFrame: DataFrame with date-like columns converted to datetime type.
+    """
+    # Copy the DataFrame to avoid modifying the original DataFrame
+    df_copy = df.copy()
+    
+    # If no specific columns are provided, select all object type columns
+    if columns is None:
+        columns = df_copy.columns[df_copy.dtypes == 'object']
+    
+    # Iterate through each specified column
+    for col in columns:
+        print(f"Attempting column: {col}")
+        # Check if the column can be converted to datetime
+        try:
+            df_copy[col] = pd.to_datetime(df_copy[col], errors='coerce')
+            print(f"Column {col} transformed successfully.\n")
+        except Exception as e:
+            print(f"Column {col} is incompatible with datetime: {e}\n")
+            # If conversion fails, skip to the next column
+            continue
+            
+    return df_copy
 
 
 
